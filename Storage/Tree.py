6ff4d9e941c9929b9fd5d4b174643efca839a18b
Karlinsky <rkarlinsky@bennington.edu>
2014-05-02 12:54:52 -0400

    made a directory

--- Storage/Tree.py
+++ Storage/Tree.py
@@ -2,4 +2,11 @@
 
 # The first attempt at coding a tree structure via python
 
+import os, sys
+
+path = "/home/rkarlinsky/GitHub/Git-Project/Storage/Tree"
+
+os.mkdir( path, 0755 );
+
+print "Path is created"
 
